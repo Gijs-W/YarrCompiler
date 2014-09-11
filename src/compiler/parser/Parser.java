2b4c12ef96856c2a8bd75d338e80d4c9d7ffe32d
Gijs-W <gijsvandewater+github@gmail.com>
2014-09-11 12:10:03 +0200

    Added JavaDoc

--- src/compiler/parser/Parser.java
+++ src/compiler/parser/Parser.java
@@ -7,12 +7,15 @@
 package compiler.parser;
 
 import compiler.token.Token;
-import compiler.token.TokenRegex;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
- *
+ * Parses tokenlist
+ * 
+ * Checks whether token list is parseble. For instance, "sina()" will result in a valid TokenSet
+ * sin, a, (, )
+ * yet it is not valid, as a function should always be followed by an (
+ * 
  * @author Gijs
  */
 public class Parser {
@@ -20,27 +23,28 @@ public class Parser {
     /**
      * Todo: DoubleLinkedListIfY this
      */
-    private final List<Token> tokenInfoList;
+    private final List<Token> tokenList;
 
-    public Parser(List<Token> tokenInfoList) {
-        this.tokenInfoList = tokenInfoList;
+    public Parser(List<Token> tokenList) {
+        this.tokenList = tokenList;
     }
     
     /**
      * @todo refactor this with DoubleLinkedList as it is easier to use
+     * @todo should contain more checks than (
      */
     public void parse() {
         Token previous = null;
-        for (Token info : tokenInfoList) {
+        for (Token token : tokenList) {
             
             if (previous != null) {
                 
-                if (previous.token == 1 && info.token != 2) {
-                    throw new RuntimeException("Parse Error: Unexpected '"  + info.sequence + "' Function must be followed by (");
+                if (previous.token == 1 && token.token != 2) {
+                    throw new RuntimeException("Parse Error: Unexpected '"  + token.value + "' Function must be followed by (");
                 }
             }
             
-            previous = info;            
+            previous = token;            
         }
     }
 }
